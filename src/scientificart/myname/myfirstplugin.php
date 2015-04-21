d92d0db6ea53e88af7eaf6476d52dfbf3b05ed42
Joshua Andre Lilleberg <mindtasticaljosh@gmail.com>
2015-04-21 17:17:04 +0200

    Update myfirstplugin.php

--- src/scientificart/myname/myfirstplugin.php
+++ src/scientificart/myname/myfirstplugin.php
@@ -1,1 +1,18 @@
-
+	<?php
+    namespace scientificart\myname;
+    
+    use pocketmine\command\Command;
+    use pocketmine\command\CommandSender;
+    use pocketmine\plugin\PluginBase;
+    use pocketmine\Player
+    use pocketmine\IPlayer
+    
+    class Main extends PluginBase{
+        public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+            switch($command->getName()){
+                case "myname";
+                2 $sender->sendMessage("Your name is " . $sender->getName() . ".");
+            }
+        }
+       
+}
